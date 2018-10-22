e35b25e80d96564cd44cc871e0a21632ba1b0755
carl <carl.rippon@theaccessgroup.com>
2018-10-22 19:06:13 +0100

    Ch 7 - last 2 sections

---
+++ 07-WorkingWithForms/04-FormSubmission/src/AdminPage.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+const AdminPage: React.SFC = () => {
+  return (
+    <div className="page-container">
+      <h1>Admin Panel</h1>
+      <p>You should only be here if you have logged in</p>
+    </div>
+  );
+};
+
+export default AdminPage;
