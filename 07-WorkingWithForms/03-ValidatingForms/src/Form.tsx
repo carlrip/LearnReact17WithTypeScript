6a7e85f3f39a1b20edee2d2b232bb60609747b04
carl <carl.rippon@theaccessgroup.com>
2018-11-17 10:49:27 +0000

    Chapter 7 - form label for fix

--- 07-WorkingWithForms/03-ValidatingForms/src/Form.tsx
+++ 07-WorkingWithForms/03-ValidatingForms/src/Form.tsx
@@ -99,7 +99,7 @@ export class Form extends React.Component<IFormProps, IState> {
       <FormContext.Consumer>
         {context => (
           <div className="form-group">
-            <label htmlFor="name">{label}</label>
+            <label htmlFor={name}>{label}</label>
             {(type === "Text" || type === "Email") && (
               <input
                 type={type.toLowerCase()}
